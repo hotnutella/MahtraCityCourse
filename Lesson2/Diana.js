5fa09816ef05d7b95c8811eafce29ff87bbcb75a
Diana <dianapatrik2@gmail.com>
2022-10-20 21:31:28 +0300

    LESSON 1 DIANA

---
+++ Lesson2/Diana.js
@@ -0,0 +1,11 @@
+var katet1 = 10;
+var katet2 = 20;
+var S = (katet1 * katet2)/2;
+console.log("Ploshad treugolnika: " + S);
+
+var radius = 23;
+var krug = radius**2 * Math.PI;
+var ploshad = Math.round(krug * 100) / 100; 
+var circle = "Ploshad kruga: "
+
+console.log(circle + ploshad);
---
+++ Lesson2/Diana2.js
@@ -0,0 +1,12 @@
+var prompt = require("prompt-sync")();
+
+var name = prompt("What is youre name?: ");
+var age = prompt("How old age you?: ");
+//console.log(name + ": " + age);
+
+
+if (age >= 18){
+    console.log(name + 'WELCOME!');
+  } else {
+    console.log(name + 'YOU SUCK ');
+  }
