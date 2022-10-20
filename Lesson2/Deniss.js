74061404fa40e1117280b65dd7e53a4208e3e266
Deniss Suhhanov <deniss.suhhanov@gmail.com>
2022-10-20 19:32:32 +0300

    Lesson 2

---
+++ Lesson2/Deniss.js
@@ -0,0 +1,10 @@
+var pivo = 10;
+var vodka = 5.645; // 15.645
+var privetstvie = "tut my napechataem peremennye";
+var skolko = "Tut 5 butylok";
+
+var summa = pivo + Number(vodka);
+Math.round(summa * 100) / 100 // 1564.5 -> 15645 -> 15.645
+console.log(summa);
+
+//NaN = Not a Number
