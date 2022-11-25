c18417018b2e58d6ab936cd7b898aa3793c96286
Diana <dianapatrik2@gmail.com>
2022-11-25 22:33:05 +0200

    1

--- Homework5/DianaTicTacToe.js
+++ Homework5/DianaTicTacToe.js
@@ -45,15 +45,55 @@ var rows = [
     ],
 ];
 
+var player1Numbers = '';
+var player2Numbers = '';
 
+var X = 'X';
+var O = 'O';
+
+var winComb = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,9],
+    [3,5,7],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9]
+];
+
+console.log('Enter number from 1 to 9!')
+
+var gameOn = true;
+while (gameOn) {
+
+if (0 < userInput < 10) {
+    for ()
+} else {
+    console.log('Enter number from 1 to 9!')
+}
+   
+
+//pokaz tablici
 for (i = 0; i < rows.length; i++) { 
     var row = rows[i];
     var visibleSlots = [];
     for (j = 0; j < row.length; j++) {
         var slot = row[j];
-        visibleSlots.push(slot.id);   
+        visibleSlots.push(slot.value);   
     }
     var visibleRow = visibleSlots.join('|')
     console.log(visibleRow);
 }
+
+var userInput = prompt('');
+   
+    if (userInput === 'stop') {
+        gameOn = false;
+        break;
+    }
+} 
+
+
+
 
