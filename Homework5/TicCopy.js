036414067eeb6ba94c2f4f7c6ca22f027bffeddd
Arvid <coldblood99999@gmail.com>
2022-11-25 21:46:24 +0200

    tic

--- Homework5/TicCopy.js
+++ Homework5/TicCopy.js
@@ -1,51 +1,70 @@
 var prompt = require("prompt-sync")();
 
-var TicTacToe = [
-    {
-        id: 1,
-        value: '',
-    },
-    {
-        id: 2,
-        value: '',
-    },
-    {
-        id: 3,
-        value: '',
-    },
-    {
-        id: 4,
-        value: '',
-    },
-    {
-        id: 5,
-        value: '',
-    },
-    {
-        id: 6,
-        value: '',
-    },
-    {
-        id: 7,
-        value: '',
-    },
-    {
-        id: 8,
-        value: '',
-    },
-    {
-        id: 9,
-        value: '',
-    },
-]
+var player1Numbers = [];
+var player2Numbers = [];
+var player1 = "";
+var player2 = "";
 
-var row = ["", "", ""];
-
-for (var i = 0; i < row.length; i++) {
-    var row1 = row[i];
-    row1 += "| ";
-    for (j = 0; j < row.length; j++) {
-        row1 += TicTacToe[j].id + " | ";
+var rows = [
+    [ 
+        {
+            id: 1,
+            value: ' ',
+        },
+        {
+            id: 2,
+            value: ' ',
+        },
+        {
+            id: 3,
+            value: ' ',
+        },
+    ],
+    [
+        {
+            id: 4,
+            value: ' ',
+        },
+        {
+            id: 5,
+            value: ' ',
+        },
+        {
+            id: 6,
+            value: ' ',
+        },
+    ],
+    [
+        {
+            id: 7,
+            value: ' ',
+        },
+        {
+            id: 8,
+            value: ' ',
+        },
+        {
+            id: 9,
+            value: ' ',
+        },
+    ],
+];
+var winComb = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,9],
+    [3,5,7],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9]
+];
+for (var row of rows) {
+    visibleRow = [];
+    for (var column of row) {
+        visibleRow.push(column.value);
     }
-    console.log(row1);
+    var newRow = visibleRow.join(" | ");
+    console.log(newRow);
 }
+
