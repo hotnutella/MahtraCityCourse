180a1d09f565d55fdc132707bc9ded458ba7db89
Dasha <darja.portnova@gmail.com>
2022-11-29 02:49:27 +0200

    hg

--- Lesson8/DashaTasks/dashaHasCatsFunctions.js
+++ Lesson8/DashaTasks/dashaHasCatsFunctions.js
@@ -1,44 +1,44 @@
 var prompt = require('prompt-sync')();
 
-var dashasCats = 2;
-function askAndSplitInput () {
-    var input = prompt('What to do with cats?: ');
-    input = input.split(' ');
-    var action = input[0];
-    var quantity = Number(input[1]);
-    function getAction (action) {
-        var actions = {
-            'nothing': stopProgram, 
-            'add': addCats, 
-            'delete': deleteCats, 
-            'show': showCats,
-        }
-    }
-    function showCats (getAction) {
-        console.log('Dasha has ' + dashasCats + ' cats.');
-    }
-    function stopProgram (getAction){
-        respawningCats = false;
-    }
-    function deleteCats (quantity) {
-        dashasCats -= quantity;
-    }
-    function addCats (quantity) {
-        dashasCats += quantity;
-    }
+// var dashasCats = 2;
+// function askAndSplitInput () {
+//     var input = prompt('What to do with cats?: ');
+//     input = input.split(' ');
+//     var action = input[0];
+//     var quantity = Number(input[1]);
+//     function getAction (action) {
+//         var actions = {
+//             'nothing': stopProgram, 
+//             'add': addCats, 
+//             'delete': deleteCats, 
+//             'show': showCats,
+//         }
+//     }
+//     function showCats (getAction) {
+//         console.log('Dasha has ' + dashasCats + ' cats.');
+//     }
+//     function stopProgram (getAction){
+//         respawningCats = false;
+//     }
+//     function deleteCats (quantity) {
+//         dashasCats -= quantity;
+//     }
+//     function addCats (quantity) {
+//         dashasCats += quantity;
+//     }
 
-}
+// }
 
-var respawningCats = true;
-while (respawningCats) {
-    askAndSplitInput;
-    console.log(dashasCats);
-    if (getAction) {
-        console.log(dashasCats);
-        continue;
-    } else {
-        console.log(dashasCats);
-        console.log('Error, try again!')
-        continue;
-    }
-}
+// var respawningCats = true;
+// while (respawningCats) {
+//     askAndSplitInput;
+//     console.log(dashasCats);
+//     if (getAction) {
+//         console.log(dashasCats);
+//         continue;
+//     } else {
+//         console.log(dashasCats);
+//         console.log('Error, try again!')
+//         continue;
+//     }
+// }
