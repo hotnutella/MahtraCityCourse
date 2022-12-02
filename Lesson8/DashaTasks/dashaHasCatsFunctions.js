5cbcb58c1ab6b54eab8a4b6fba11a0681c50d854
Dasha <darja.portnova@gmail.com>
2022-12-03 01:52:48 +0200

    DashaHasCats with functions

--- Lesson8/DashaTasks/dashaHasCatsFunctions.js
+++ Lesson8/DashaTasks/dashaHasCatsFunctions.js
@@ -1,84 +1,41 @@
 var prompt = require('prompt-sync')();
 
 var dashasCats = 2;
-var action = input[0];
-var quantity = Number(input[1]);
+var respawningCats = true;
 
-
-function input () {
+function getActionFromInput () {
     var input = prompt('What to do with cats?: ');
     input = input.split(' ');
-    return action && quantity;
-    
-}
-
-var actions = [
-    {'add': addCats,},
-    {'delete': deleteCats,},
-    {'show': showCats,},
-    {'nothing': stopProgram,}
-]
-function getActions() {
-    if (actions.includes(action)) {
-        return console.log('here vot tut')
+    var actions = getAction();
+    var result = {
+        action: actions[input[0]],
+        quantity: Number(input[1]),
     }
+    return result;
 }
-/*
-function getActions() {
+function getAction() {
     var actions = {
-        'add': addCats,
-        'delete': deleteCats,
-        'show': showCats,
-        'nothing': stopProgram,
+        add: addCats,
+        delete: deleteCats,
+        show: showCats,
+        nothing: stopProgram,
     }
-
-    console.log('Here 2');
     return actions;
 }
-
-function testkeys() {
-    var actions = getActions();
-        console.log('done!2');
-    return Object.keys(actions); 
-}
-function testsave() {
-    var actions = getActions();
-    if (actions.includes(action)) {
-        console.log('done!');
-    }
-}
-*/
-
-
-
-function addCats (dashasCats, quantity) {
+function addCats (quantity) {
     dashasCats += quantity;
-    console.log('Here 3');
-    return dashasCats;
 }
-function deleteCats (dashasCats, quantity) {
+function deleteCats (quantity) {
     dashasCats -= quantity;
-    console.log('Here 4');
-    return dashasCats;
 }
 function showCats () {
-    console.log('Here 5');
-    return console.log('Dasha has ' + dashasCats + ' cats');
+    console.log('Dasha has ' + dashasCats + ' cats');
 }
-function stopProgram (action) {
-    action === 'nothing';
-    console.log('Here 6');
-    return respawningCats = false;
+function stopProgram () {
+    respawningCats = false;
 }
 
-
-var respawningCats = true;
 while (respawningCats) {
-    input();
-    if (getActions()) {
-        continue;
-    } else {
-        console.log('Error, try again!');
-        break;
-    }
+    var actionObject = getActionFromInput();
+    actionObject.action(actionObject.quantity);
 }
