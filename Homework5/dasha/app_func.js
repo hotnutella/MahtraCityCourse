8c7bb1711c38c93a67720e794e2637e46d2c8f92
Dasha <darja.portnova@gmail.com>
2022-12-27 01:58:00 +0200

    Finalise TicTacToe

--- Homework5/dasha/app_func.js
+++ Homework5/dasha/app_func.js
@@ -1,72 +1,113 @@
 import PromptSync from "prompt-sync";
 import config from "./config.js";
-var prompt = PromptSync();
+const prompt = PromptSync();
 
-var currentPlayer;
-var playerX = [];
-var playerO = [];
-var move = 1;
-var userInput;
-var isGameOver = false;
-
-var theSlot = config.rows.map(function (row) {
-    return row;
-})
-.map(function (slot) {
-    slot = slot.value
-    return slot;
-})
+const playerX = [];
+const playerO = [];
+let move = 1;
+let isGameOver = false;
 
 function visibleSlots () {
-    for (var row of config.rows) {
-        var visibleSlot = [];
-        for (var slot of row) {
+    for (const row of config.rows) {
+        const visibleSlot = [];
+        for (const slot of row) {
             visibleSlot.push(slot.value);
         }
-        var visibleRow = visibleSlot.join('|');
+        const visibleRow = visibleSlot.join('|');
         console.log(visibleRow);   
     }
 }
 function askID () {
-    var inputSlotID = prompt('Please, input number from 1 to 9: ');
+    let inputSlotID = prompt('Please, input number from 1 to 9: ');
     inputSlotID = Number(inputSlotID);
-    return move++;
+    move++
+    return inputSlotID;
 }
 function slotCheck (inputSlotID) {
-    if (theSlot.id === inputSlotID) {
-        currentPlayerAndMoves();  
-    } else {
-        visibleSlots.push(config.slot.value);
+    let slot = getSlotByID(inputSlotID);
+    currentPlayerAndMoves(slot);  
+}
+function getSlotByID (slotID) {
+    for (const row of config.rows) {
+        for (const slot of row) {
+            if (slotID === slot.id) {
+                return slot;
+            }
+        }
     }
 }
-function currentPlayerAndMoves (inputSlotID, theSlot) {
-    if (Number.isInteger(move) == true) {
-        if (move % 2 == 0) {
-            currentPlayer = 'X';
-            playerX.push(inputSlotID);
-        } else {
-            currentPlayer = 'O'
-            playerO.push(inputSlotID);
-        }
-        theSlot = currentPlayer;
-    } 
+function currentPlayerAndMoves (slot) {
+    let currentPlayer;
+    if (move % 2 == 0) {
+        currentPlayer = 'X';
+        playerX.push(slot.id);
+    } else {
+        currentPlayer = 'O'
+        playerO.push(slot.id);
+    }
+    slot.value = currentPlayer;
 }
-// function winCheck () {
-//     var winner = config.winningCombinations.filter(function(win) {
-//         if (win ===) {
-//             if () {
-//                 console.log('Game Over?');
-//                 isGameOver = true;
-//             }
-//         }
-//     })
-//     return winner;
-// }
+function winCheck () {
+    // for (const win of config.winningCombinations) {
+    //     playerX.sort((a, b) => a - b);
+    //     playerO.sort((a, b) => a - b);
+    //     // const winnerBla = "1,7,8,9".includes("7,8,9");
+    //     const winnerX = JSON.stringify(playerX).includes(JSON.stringify(win));
+    //     const winnerO = JSON.stringify(playerO).includes(JSON.stringify(win));
+    //     console.log('win', win);
+    //     console.log('player x', playerX);
+    //     console.log('player o', playerO);
+    //     if (winnerX) {
+    //         winner = 'X';
+    //     } else if (winnerO) {
+    //         winner = 'O';
+    //     }
+    //     if (winner) {
+    //         const victory = 'VICTORY!!! The winner is ' + winner + '!';
+    //         console.log(victory);
+    //         gameOver();
+    //         break;
+    //     }
+    // }
+    let winner;
+    if (isWin(playerX)) {
+        winner = 'X';
+    } else if (isWin(playerO)) {
+        winner = 'O'
+    }
+    if (winner) {
+        const victory = 'VICTORY!!! The winner is ' + winner + '!';
+        console.log(victory);
+        gameOver();
+    }
+    if (move === 10) {
+        gameOver();
+    }
+}
+function isWin (player) {
+    let isWin = false;
+    for (const win of config.winningCombinations) {
+        let slotCount = 0;
+        for (const slotId of player) {
+            if (win.includes(slotId)) {
+                slotCount++;
+            }
+        }
+        if (slotCount ===  win.length) {
+            isWin = true;
+        }
+    }
+    return isWin;
+}
+function gameOver () {
+    move = 1;
+    console.log('Game over!')
+    isGameOver = true;
+}
 while (!isGameOver) {
     visibleSlots();
-    userInput = askID();
-    slotCheck();
-
+    const userInput = askID();
+    slotCheck(userInput);
     winCheck();
-    console.log('here')
+    
 }
